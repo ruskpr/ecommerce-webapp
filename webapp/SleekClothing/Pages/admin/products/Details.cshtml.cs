eacd3698a5d211d1444645cb01b48be19bf14d2d
Rustu Koprulu <rustulp@gmail.com>
2023-03-31 23:16:28 -0600

    added

---
+++ webapp/SleekClothing/Pages/admin/products/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SleekClothing.Data;
+using SleekClothing.Models;
+
+namespace SleekClothing.Pages.admin.products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SleekClothing.Data.ApplicationDbContext _context;
+
+        public DetailsModel(SleekClothing.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+      public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
---
+++ webapp/SleekClothing/Pages/admin/products/Index.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SleekClothing.Data;
+using SleekClothing.Helpers;
+using SleekClothing.Models;
+
+namespace SleekClothing.Pages.admin.products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SleekClothing.Data.ApplicationDbContext _context;
+
+        public IndexModel(SleekClothing.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Products { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Products = await _context.Products.ToListAsync();
+            }
+        }
+
+
+        public IActionResult OnPostAddToCart(int productId)
+        {
+            Products = _context.Products.ToList();
+
+            Product product = _context.Products.First(x => x.Id == productId);
+
+            //handle product out of stock
+            if (product.IsOutOfStock) return Redirect("/products");
+
+            var u = UsersHelper.GetUser(_context, this.User);
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                CartHelper.AddToCartCookie(product, HttpContext);
+            }
+            else
+            {
+                CartHelper.AddToCartDb(product, _context, this.User);
+            }
+
+            return Redirect("/products");
+        }
+
+        public IActionResult OnPostAddToWishlist(int productId)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return Redirect("/Identity/Account/Login");
+
+            Product product = _context.Products.First(x => x.Id == productId);
+
+            WishlistHelper.AddToWishlist(product, _context, this.User);
+
+            Products = _context.Products.ToList();
+            return Redirect("/products");
+        }
+
+
+    }
+}
